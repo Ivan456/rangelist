7e5d0eae42fcbb18a2c38fcd73b027d6a3846316
Ivan Vodzich <Ivan_Vodzich@epam.com>
2019-07-18 21:25:27 +0300

    all tests pass

--- RangeList.js
+++ RangeList.js
@@ -1,67 +1,66 @@
-const INSIDE_RANGE = 'INSIDE_RANGE';
-const AFTER_RANGE = 'AFTER_RANGE';
-
 module.exports = class RangeList {
     constructor() {
         this.list = [];
     }
+
+    isValidRange(left, right) {
+        return left < right;
+    };
 
     /**
      * Adds a range to the list
      * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
      */
     add([left, right]) {
-        if (left === right) return;
+        if (!this.isValidRange(left, right)) return;
 
         right--;
-        if (!this.list.length) return this.list.push([left, right]);
+        if (!this.list.length) return this.list.push({ left, right });
 
         let leftPosition = this.definePosition(left);
-        let rightPosition = this.definePosition(right);
+        let rightPosition = this.definePosition(right, leftPosition.value);
         let positionDiff = rightPosition.value - leftPosition.value;
 
-        switch (leftPosition.name + rightPosition.name) {
-            case INSIDE_RANGE + INSIDE_RANGE:
-                if (left === right) {
-                    if (left < this.list[leftPosition.value][0]) {
-                        return this.list[leftPosition.value][0] = left;
-                    }
-                    return this.list[leftPosition.value][1] = right;
-                }
+        this.extendBorders(left, right, leftPosition, rightPosition);
 
-                this.list[rightPosition.value][0] = this.list[leftPosition.value][0];
-                this.list.splice(leftPosition.value, positionDiff);
-                break;
-            case AFTER_RANGE + INSIDE_RANGE:
-                this.list[rightPosition.value][0] = left;
-                this.list.splice(leftPosition.value + 1, positionDiff - 1);
-                break;
-            case INSIDE_RANGE + AFTER_RANGE:
-                this.list[leftPosition.value][1] = right;
-                this.list.splice(leftPosition.value + 1, positionDiff);
-                break;
-            case AFTER_RANGE + AFTER_RANGE:
-                if (leftPosition.value === rightPosition.value) {
-                    return this.list.splice(leftPosition.value + 1, 0, [ left, right ]);
-                }
-
-                this.list[leftPosition.value + 1][0] = left;
-                this.list[leftPosition.value + 1][1] = right;
-                this.list.splice(leftPosition.value + 2, positionDiff - 1);
-                break;
+        if (leftPosition.inside && rightPosition.inside) {
+            this.list[rightPosition.value].left = this.list[leftPosition.value].left;
+            this.list.splice(leftPosition.value, positionDiff);
+        } else if (!leftPosition.inside && rightPosition.inside) {
+            this.list[rightPosition.value].left = left;
+            this.list.splice(leftPosition.value + 1, positionDiff - 1);
+        } else if (leftPosition.inside && !rightPosition.inside) {
+            this.list[leftPosition.value].right = right;
+            this.list.splice(leftPosition.value + 1, positionDiff);
+        } else {
+            if (leftPosition.value === rightPosition.value) {
+                return this.list.splice(leftPosition.value + 1, 0, { left, right });
+            }
+            this.list[leftPosition.value + 1].left = left;
+            this.list[leftPosition.value + 1].right = right;
+            this.list.splice(leftPosition.value + 2, positionDiff - 1);
         }
     }
 
-    definePosition(value) {
-        let i = 0;
+    extendBorders(left, right, leftPosition, rightPosition) {
+        if (leftPosition.value === -1) return;
 
-        if (value < this.list[0][0] - 1) return { value: -1, name: AFTER_RANGE };
-        if (value > this.list[this.list.length - 1][1] + 1) return { value: this.list.length - 1, name: AFTER_RANGE };
+        if (left < this.list[leftPosition.value].left && leftPosition.inside) {
+            this.list[leftPosition.value].left = left;
+        }
+        if (right > this.list[rightPosition.value].right && rightPosition.inside) {
+            this.list[rightPosition.value].right = right;
+        }
+    }
+
+    definePosition(value, i = 0) {
+        if (i === -1) i = 0;
+        if (value < this.list[0].left - 1) return { value: -1, inside: false };
+        if (value > this.list[this.list.length - 1].right + 1) return { value: this.list.length - 1, inside: false };
 
         while (i < this.list.length) {
-            if (this.list[i][0] - 1 <= value && value <= this.list[i][1] + 1) return { value: i, name: INSIDE_RANGE };
-            if (this.list[i][1] + 1 < value && value < this.list[i + 1][0] - 1) return { value: i, name: AFTER_RANGE };
-
+            if (this.list[i].left - 1 <= value && value <= this.list[i].right + 1) return { value: i, inside: true };
+            if (this.list[i].right + 1 < value && value < this.list[i + 1].left - 1) return { value: i, inside: false };
             i++;
         }
     }
@@ -71,69 +70,61 @@ module.exports = class RangeList {
      * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
      */
     remove([left, right]) {
-        if (!this.list.length || left === right) return;
+        if (!this.list.length || this.isValidRange(left, right)) return;
         right--;
 
         let leftPosition = this.defineRemovePosition(left);
-        let rightPosition = this.defineRemovePosition(right);
+        let rightPosition = this.defineRemovePosition(right, leftPosition.value);
         let positionDiff = rightPosition.value - leftPosition.value;
 
         this.removeBorders(rightPosition.value, right);
         this.removeBorders(leftPosition.value, left);
 
-        switch (leftPosition.name + rightPosition.name) {
-            case INSIDE_RANGE + INSIDE_RANGE:
-                if (leftPosition.value === rightPosition.value) {
-                    this.list.splice(leftPosition.value + 1, 0, [right + 1, this.list[leftPosition.value][1]]);
-                    this.list[leftPosition.value][1] = left - 1;
-                    break;
-                }
-
-                this.list[leftPosition.value][1] = left - 1;
-                this.list[rightPosition.value][0] = right + 1;
-                this.list.splice(leftPosition.value + 1, positionDiff - 1);
-                break;
-            case AFTER_RANGE + INSIDE_RANGE:
-                this.list[rightPosition.value][0] = right + 1;
-                this.list.splice(leftPosition.value + 1, positionDiff - 1);
-                break;
-            case INSIDE_RANGE + AFTER_RANGE:
-                this.list[leftPosition.value][1] = left - 1;
-                this.list.splice(leftPosition.value + 1, positionDiff);
-                break;
-            case AFTER_RANGE + AFTER_RANGE:
-                if (leftPosition.value === rightPosition.value) {
-                    break;
-                }
-                this.list.splice(leftPosition.value + 1, positionDiff - 1);
-                break;
+        if (leftPosition.inside && rightPosition.inside) {
+            if (leftPosition.value === rightPosition.value) {
+                this.list.splice(leftPosition.value + 1, 0, {
+                    left: right + 1,
+                    right: this.list[leftPosition.value].right
+                });
+                this.list[leftPosition.value].right = left - 1;
+                return;
+            }
+            this.list[leftPosition.value].right = left - 1;
+            this.list[rightPosition.value].left = right + 1;
+            this.list.splice(leftPosition.value + 1, positionDiff - 1);
+        } else if (!leftPosition.inside && rightPosition.inside) {
+            this.list[rightPosition.value].left = right + 1;
+            this.list.splice(leftPosition.value + 1, positionDiff - 1);
+        } else if (leftPosition.inside && !rightPosition.inside) {
+            this.list[leftPosition.value].right = left - 1;
+            this.list.splice(leftPosition.value + 1, positionDiff);
+        } else {
+            if (leftPosition.value === rightPosition.value) return;
+            this.list.splice(leftPosition.value + 1, positionDiff - 1);
         }
     }
 
-    defineRemovePosition(value) {
-        let i = 0;
-
-        if (value < this.list[0][0]) return { value: -1, name: AFTER_RANGE };
-        if (value > this.list[this.list.length - 1][1]) return { value: this.list.length - 1, name: AFTER_RANGE };
+    defineRemovePosition(value, i = 0) {
+        if (i === -1) i = 0;
+        if (value < this.list[0].left) return { value: -1, inside: false };
+        if (value > this.list[this.list.length - 1].right) return { value: this.list.length - 1, inside: false };
 
         while (i < this.list.length) {
-            if (this.list[i][0] < value && value < this.list[i][1]) return { value: i, name: INSIDE_RANGE };
-            if (this.list[i][1] <= value && value <= this.list[i + 1][0]) 
-            return { value: i, name: AFTER_RANGE };
-
+            if (this.list[i].left < value && value < this.list[i].right) return { value: i, inside: true };
+            if (this.list[i].right <= value && value <= this.list[i + 1].left) return { value: i, inside: false };
             i++;
         }
     }
 
     removeBorders(index, value) {
-        if (this.list[index][0] === value) {
-            this.list[index][0]++;
+        if (this.list[index].left === value) {
+            this.list[index].left++;
         }
-        if (this.list[index][1] === value) {
-            this.list[index][1]--;
+        if (this.list[index].right === value) {
+            this.list[index].right--;
         }
-        if (this.list[index + 1] && this.list[index + 1][0] === value) {
-            this.list[index + 1][0]++;
+        if (this.list[index + 1] && this.list[index + 1].left === value) {
+            this.list[index + 1].left++;
         }
     }
 
@@ -141,12 +132,12 @@ module.exports = class RangeList {
      * Prints out the list of ranges in the range list
      */
     print() {
-        const output = this.list.map(([left, right]) => {
+        console.log(this.getList());
+    }
+
+    getList() {
+        return this.list.map(({ left, right}) => {
             return `[${left}, ${right + 1})`;
         }).join(' ');
-
-        console.log(output);
-
-        return output;
     }
-  }
+}
--- test/test.js
+++ test/test.js
@@ -6,43 +6,34 @@ describe('basic test', () => {
 
     it('basic', () => {
         rl.add([1, 5]);
-        assert.equal(rl.print(), '[1, 5)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 5)');
+
         rl.add([10, 20]);
-        assert.equal(rl.print(), '[1, 5) [10, 20)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 5) [10, 20)');
+
         rl.add([20, 20]);
-        assert.equal(rl.print(), '[1, 5) [10, 20)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 5) [10, 20)');
+
         rl.add([20, 21]);
-        assert.equal(rl.print(), '[1, 5) [10, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 5) [10, 21)');
+
         rl.add([2, 4]);
-        assert.equal(rl.print(), '[1, 5) [10, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 5) [10, 21)');
+
         rl.add([3, 8]);
-        assert.equal(rl.print(), '[1, 8) [10, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 8) [10, 21)');
+
         rl.remove([10, 10]);
-        assert.equal(rl.print(), '[1, 8) [10, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 8) [10, 21)');
+
         rl.remove([10, 11]);
-        assert.equal(rl.print(), '[1, 8) [11, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 8) [11, 21)');
+
         rl.remove([15, 17]);
-        assert.equal(rl.print(), '[1, 8) [11, 15) [17, 21)');
-    });
-    it('basic', () => {
+        assert.equal(rl.getList(), '[1, 8) [11, 15) [17, 21)');
+
         rl.remove([3, 19]);
-        assert.equal(rl.print(), '[1, 3) [19, 21)');
+        assert.equal(rl.getList(), '[1, 3) [19, 21)');
     });
 });
 
@@ -50,64 +41,44 @@ describe('basic test', () => {
 describe('extended test', () => {
     const rl = new RL();
 
-    it('extended', () => {
+    it('extended ', () => {
         rl.add([0, 0]);
-        assert.equal(rl.print(), '');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '');
+
         rl.add([-1, -1]);
-        assert.equal(rl.print(), '');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '');
+
         rl.add([-1, 0]);
-        assert.equal(rl.print(), '[-1, 0)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-1, 0)');
+
         rl.add([-10, -1]);
-        assert.equal(rl.print(), '[-10, 0)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-10, 0)');
+
         rl.add([-11, 1]);
-        assert.equal(rl.print(), '[-11, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-11, 1)');
+
         rl.add([-11, -11]);
-        assert.equal(rl.print(), '[-11, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-11, 1)');
+
         rl.add([-12, -12]);
-        assert.equal(rl.print(), '[-11, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-11, 1)');
+
         rl.add([-100, -11]);
-        assert.equal(rl.print(), '[-100, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-100, 1)');
+
         rl.add([-400, -300]);
-        assert.equal(rl.print(), '[-400, -300) [-100, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-400, -300) [-100, 1)');
+
         rl.add([-200, -150]);
-        assert.equal(rl.print(), '[-400, -300) [-200, -150]) [-100, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-400, -300) [-200, -150) [-100, 1)');
+
         rl.remove([-300, -200]);
-        assert.equal(rl.print(), '[-400, -300) [-200, -150]) [-100, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-400, -300) [-200, -150) [-100, 1)');
+
         rl.remove([-301, -199]);
-        assert.equal(rl.print(), '[-400, -301) [-199, -150]) [-100, 1)');
-    });
-    it('extended', () => {
+        assert.equal(rl.getList(), '[-400, -301) [-199, -150) [-100, 1)');
+
         rl.remove([-399, -148]);
-        assert.equal(rl.print(), '[-400, -399) [-149, -150]) [-100, 1)');
+        assert.equal(rl.getList(), '[-400, -399) [-100, 1)');
     });
-    // it('extended', () => {
-    //     rl.remove([15, 17]);
-    //     assert.equal(rl.print(), '[1, 8) [11, 15) [17, 21)');
-    // });
-    // it('extended', () => {
-    //     rl.remove([3, 19]);
-    //     assert.equal(rl.print(), '[1, 3) [19, 21)');
-    // });
 });
